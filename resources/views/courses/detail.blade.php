9bc8077072866733d50a9270e5ddfad91412fbd8
Owen <145958069+0wLzz@users.noreply.github.com>
2025-04-03 16:49:18 +0700

    courseDetail

---
+++ resources/views/courses/detail.blade.php
@@ -0,0 +1,213 @@
+<x-layout>
+    <section class="bg-white dark:bg-gray-900 h-min-screen items-center pb-8">
+        <div class="max-w-screen-xl mx-auto">
+            <div class="grid grid-cols-2">
+                <div class="mx-auto max-w-sm my-24 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                    {{-- Card --}}
+                    <a href="#">
+                        <img class="rounded-t-lg" src="{{asset('assets/AboutUs.png')}}" alt=""/>
+                    </a>
+                    <div class="p-5 text-white">
+                        <p class="">Benefit Kelas</p>
+                        <ol class="mb-3">
+                            <li>1. Materi Leadership</li>
+                            <li>2. Materi Leadership</li>
+                            <li>3. Materi Leadership</li>
+                            <li>4. Materi Leadership</li>
+                        </ol>
+                
+                
+                        <div class=" grid grid-cols-4 place-items-center">
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"/>
+                            </svg>
+                            
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-width="2" d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                            </svg>
+                
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"/>
+                            </svg>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+                            </svg>
+                              
+                            <p class="font-normal text-gray-700 dark:text-white">100</p>
+                            <p class="font-normal text-gray-700 dark:text-white">100</p>
+                            <p class="font-normal text-gray-700 dark:text-white">100</p>
+                            <p class="font-normal text-gray-700 dark:text-white">Chat</p>
+                        </div>
+                    </div>
+                    <div class="border-t-2 p-8">
+                        <p class="text-white">Rp. 500.000</p>
+                        <button type="submit" class="p-2.5 w-full text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <span>DAFTAR KELAS</span>
+                        </button>
+                    </div>
+                    {{-- End Card --}}
+                </div>
+
+                <div class="text-white my-24 flex flex-col gap-2">
+                    <h1 class="text-4xl">Belajar Membangun Koneksi dengan orang yang belum dikenal</h1>
+                    <p class="text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab nobis, velit omnis doloremque animi repellat a sunt quis natus eius placeat dolorem voluptate quia sint consequatur? Quas illo beatae distinctio Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis accusamus laborum atque illum numquam, dignissimos beatae minima unde voluptate eaque cum delectus at, voluptates sunt, earum perferendis sed nam eum!
+                    </p>
+                    
+                    <div class="flex items-center self-end">
+                        <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                        </svg>
+                        <p class="ms-2 text-sm font-bold text-gray-900 dark:text-white">4.95</p>
+                        <span class="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
+                        <a href="#" class="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white">73 reviews</a>
+                    </div>
+
+                    <p class="text-2xl">Description</p>
+                    <p class="text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab nobis, velit omnis doloremque animi repellat a sunt quis natus eius placeat dolorem voluptate quia sint consequatur? Quas illo beatae distinctio Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis accusamus laborum atque illum numquam, dignissimos beatae minima unde voluptate eaque cum delectus at, voluptates sunt, earum perferendis sed nam eum! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis eveniet, similique autem laboriosam ratione expedita nesciunt unde id ut hic odit! Hic excepturi ex ducimus officiis at, similique magni reiciendis. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni repellat culpa quam quidem ducimus facere similique omnis. Quo quas, eius ullam a est officiis, numquam pariatur quod non illo saepe.
+                    </p>
+                </div>
+            </div>
+
+            <div class="text-white mb-8">
+                <h2 class="text-3xl font-bold mb-4">Tutor</h2>
+                <div class="flex items-center mb-4">
+                    <img class="w-50 h-50 rounded-full aspect-square object-cover" src="{{asset('assets/Carousel2.jpg')}}" alt="Rounded avatar">
+                    <div class="flex flex-col ml-4 text-2xl">
+                        <p class="font-bold"> Ambda Jansen </p>
+                        <p class="text-blue-400"> Communication Expert </p>
+                    </div>
+                </div>
+                <p class="text-gray-400 text-xl">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium aut officia, modi odit minima iusto eos incidunt ad amet ullam optio accusantium voluptatibus expedita ducimus necessitatibus voluptates quas esse iure. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis et autem debitis laboriosam magnam totam ducimus corrupti nisi eos. Dolor obcaecati necessitatibus alias facilis iusto illum a assumenda tempora eum?
+                </p>
+            </div>
+
+            {{-- Materi  --}}
+            <div class="flex justify-center text-white bg-gray-700">
+                <h1 class="text-3xl font-bold">MATERI</h1>
+            </div>
+            
+            <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
+                <h2 id="accordion-flush-heading-1">
+                <button type="button" class="flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 gap-3" data-accordion-target="#accordion-flush-body-1" aria-expanded="true" aria-controls="accordion-flush-body-1">
+                    <span>What is Flowbite?</span>
+                    <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+                    </svg>
+                </button>
+                </h2>
+                <div id="accordion-flush-body-1" class="hidden" aria-labelledby="accordion-flush-heading-1">
+                <div class="py-5 border-b border-gray-200 dark:border-gray-700">
+                    <p class="mb-2 text-gray-500 dark:text-gray-400">Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
+                    <p class="text-gray-500 dark:text-gray-400">Check out this guide to learn how to <a href="/docs/getting-started/introduction/" class="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p>
+                </div>
+                </div>
+                <h2 id="accordion-flush-heading-2">
+                <button type="button" class="flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 gap-3" data-accordion-target="#accordion-flush-body-2" aria-expanded="false" aria-controls="accordion-flush-body-2">
+                    <span>Is there a Figma file available?</span>
+                    <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+                    </svg>
+                </button>
+                </h2>
+                <div id="accordion-flush-body-2" class="hidden" aria-labelledby="accordion-flush-heading-2">
+                <div class="py-5 border-b border-gray-200 dark:border-gray-700">
+                    <p class="mb-2 text-gray-500 dark:text-gray-400">Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.</p>
+                    <p class="text-gray-500 dark:text-gray-400">Check out the <a href="https://flowbite.com/figma/" class="text-blue-600 dark:text-blue-500 hover:underline">Figma design system</a> based on the utility classes from Tailwind CSS and components from Flowbite.</p>
+                </div>
+                </div>
+                <h2 id="accordion-flush-heading-3">
+                <button type="button" class="flex items-center justify-between w-full py-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 gap-3" data-accordion-target="#accordion-flush-body-3" aria-expanded="false" aria-controls="accordion-flush-body-3">
+                    <span>What are the differences between Flowbite and Tailwind UI?</span>
+                    <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+                    </svg>
+                </button>
+                </h2>
+                <div id="accordion-flush-body-3" class="hidden" aria-labelledby="accordion-flush-heading-3">
+                <div class="py-5 border-b border-gray-200 dark:border-gray-700">
+                    <p class="mb-2 text-gray-500 dark:text-gray-400">The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.</p>
+                    <p class="mb-2 text-gray-500 dark:text-gray-400">However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.</p>
+                    <p class="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+                    <ul class="ps-5 text-gray-500 list-disc dark:text-gray-400">
+                    <li><a href="https://flowbite.com/pro/" class="text-blue-600 dark:text-blue-500 hover:underline">Flowbite Pro</a></li>
+                    <li><a href="https://tailwindui.com/" rel="nofollow" class="text-blue-600 dark:text-blue-500 hover:underline">Tailwind UI</a></li>
+                    </ul>
+                </div>
+                </div>
+            </div>
+
+            <div id="category-carousel" class="items-center relative w-full text-center bg-gray-800 rounded drop-shadow-xl text-gray-500 mb-6 sm:text-lg dark:text-gray-400">
+                <!-- Category Display -->
+                <div class="relative h-16 flex items-center justify-center text-2xl font-bold gap-24">
+                    <span id="category-label-1" class="text-blue-400">Leadership</span>
+                    <span id="category-label-2" class="">Communication</span>
+                    <span id="category-label-3" class="">Problem Solving</span>
+                    <span id="category-label-4" class="">Time Managment</span>
+                </div>
+
+                <!-- Slider controls -->
+                <button id="prev-category" type="button" class="absolute left-0 top-1/2 transform -translate-y-1/2 px-4 py-2">
+                    <svg class="w-4 h-4 text-white dark:text-gray-400 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                    </svg>
+                </button>
+                
+                <button id="next-category" type="button" class="absolute right-0 top-1/2 transform -translate-y-1/2 px-4 py-2">
+                    <svg class="w-4 h-4 text-white dark:text-gray-400 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                    </svg>
+                </button>
+            </div>
+
+            <div class="grid grid-cols-4 gap-8 gap-y-10 mb-8">
+                <x-card.course />
+                <x-card.course />
+                <x-card.course />
+                <x-card.course />
+ 
+            </div>
+
+            <nav aria-label="Page navigation example">
+                <ul class="flex justify-center items-center -space-x-px h-10 text-base">
+                <li>
+                    <a href="#" class="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    <span class="sr-only">Previous</span>
+                    <svg class="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                    </svg>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+                </li>
+                <li>
+                    <a href="#" aria-current="page" class="z-10 flex items-center justify-center px-4 h-10 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    <span class="sr-only">Next</span>
+                    <svg class="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                    </svg>
+                    </a>
+                </li>
+                </ul>
+            </nav>
+  
+
+        </div>
+
+        
+    </section>
+</x-layout>
